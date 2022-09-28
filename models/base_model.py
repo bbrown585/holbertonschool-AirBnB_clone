c7bfd4e9bc3194c685a8e3e082c22edfe1220ed2
JamesTeeters <4925@holbertonstudents.com>
2022-09-28 14:31:06 -0500

    'updated BaseModel class with kwargs and fixed dictionary return to make all keys strings(fixed output)'

--- models/base_model.py
+++ models/base_model.py
@@ -8,16 +8,30 @@ import models
 
 class BaseModel:
     """created Class for BaseModel"""
-    def __init__(self) -> None:
+    def __init__(self, *args, **kwargs):
         """public instance attributes for BaseModel"""
-        time_format = '%Y-%m-%dT%H:%M:%S.%f'
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.today()
-        self.updated_at = datetime.datetime.today()
+        timedate_format = '%Y-%m-%dT%H:%M:%S.%f'
+        if kwargs is not None and kwargs != {}:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    break
+                elif key == "id":
+                    self.id = value
+                elif key == "created_at":
+                    self.created_at = datetime.\
+                        datetime.strptime(value, timedate_format)
+                elif key == "updated_at":
+                    self.updated_at = datetime.\
+                        datetime.strptime(value, timedate_format)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.today()
+            self.updated_at = datetime.datetime.today()
 
     def __str__(self) -> str:
         """String override for BaseModel"""
-        return "[{}] ({}) {}".format(__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """updates (updated_at attribute)to current time"""
@@ -25,9 +39,8 @@ class BaseModel:
 
     def to_dict(self):
         """returns a dictionary of Class instance"""
-        self.__dict__[__class__] = self.__class__.__name__
-        self.__dict__["created_at"] = self.created_at.isoformat
-        self.__dict__["updated_at"] = self.updated_at.isoformat
-        return self.__dict__
-
-    
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = self.__class__.__name__
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        return my_dict
