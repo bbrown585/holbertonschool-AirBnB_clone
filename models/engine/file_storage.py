6e5b1f6108eebe3b403b2e2766716f8814062dca
bbrown585 <brandon_scott585@yahoo.com>
2022-10-02 09:42:57 -0700

    filestorage class

---
+++ models/engine/file_storage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""FileStorage that serializes instances to a JSON file and deserializes JSON file to instances:"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage():
+     """ serializes instances to a JSON file and deserializes JSON"""
+    
+     __file_path = 'file.json'
+     __objects = {}
+     
+    def all(seff):
+        """returns the dictionary __objects"""
+        return self.__objects
+    
+    def new(self, obj):
+        """ sets in __objects the obj with key <obj class name>.id"""
+
